401346bb10c91b6b53c503761888ed9e4fb04f12
Joshua S. Toy <joshua.toy@ingramcontent.com>
2018-11-18 18:02:31 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,219 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import Name from "./name";
+import regular from "./fonts/regular.ttf";
+import schwifty from "./fonts/schwifty.ttf";
+import beth from "./chars/beth.png";
+import Jerry from "./chars/Jerry.png";
+import bird from "./chars/beth.png";
+import gas from "./chars/gas.png";
+import hammer from "./chars/hammer.png";
+import seek from "./chars/seek.png";
+import Squanchy from "./chars/Squanchy.png";
+import pickle from "./chars/pickle.png";
+import morty from "./chars/beth.png";
+import summer from "./chars/summer.png";
+import gum from "./chars/gum.png";
 
-class App extends Component {
+const NameContainer = styled.div`
+  font-family: "schwifty";
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 15px;
+  text-shadow: -1px 0 #44bd32, 0 1px #4cd137, 1px 0 #4cd137, 0 -1px #4cd137;
+  .char {
+    display: block;
+    width: auto;
+    max-width: 100%;
+    max-height: 250px;
+  }
+`;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: -webkit-linear-gradient(
+    -23deg,
+    rgb(162, 220, 220),
+    rgb(43, 156, 199)
+  );
+  background: linear-gradient(-23deg, rgb(162, 220, 220), rgb(43, 156, 199));
+  @font-face {
+    font-family: "regular";
+    src: url(${regular}) format("truetype");
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: "schwifty";
+    src: url(${schwifty}) format("truetype");
+    font-weight: normal;
+    font-style: normal;
+  }
+  font-family: "regular";
+`;
+
+const Horoscope = styled.ul`
+  color: #dcdde1;
+  font-size: 23px;
+  text-align: center;
+  padding: 25px;
+  list-style: none;
+  .color {
+    border: 3px dashed #7f8fa6;
+    padding: 5px;
+    margin: 10px;
+  }
+`;
+
+const Bubble = styled.div`
+  position: relative;
+  background: #cdff00;
+  border-radius: 0.4em;
+  width: 60%;
+  padding: 10px;
+  margin: 20px;
+  font-size: 19px;
+
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border: 73px solid transparent;
+    border-top-color: #cdff00;
+    border-bottom: 0;
+    border-right: 0;
+    margin-left: -36.5px;
+    margin-bottom: -73px;
+  }
+`;
+
+const Description = styled.div`
+  .gum {
+    float: right;
+    right: 0;
+    bottom: 0;
+  }
+`;
+
+const ColorBox = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: ${props => (props.color ? props.color : null)};
+  border: 2px solid black;
+  border-radius: 50px;
+  margin: 0 auto;
+`;
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: false,
+      json: {}
+    };
+  }
+
+  apiCall(name, zodiac) {
+    zodiac = zodiac.split(":")[0];
+
+    const URL = `https://aztro.sameerkumar.website/?sign=${zodiac}&day=today`;
+    fetch(URL, {
+      method: "POST"
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ json, name, zodiac });
+      });
+  }
+
+  getHoroscope = userInfo => {
+    this.apiCall(userInfo.name, userInfo.zodiac);
+  };
+
+  getImage = userInfo => {
+    const images = [
+      beth,
+      Jerry,
+      bird,
+      gas,
+      hammer,
+      seek,
+      Squanchy,
+      pickle,
+      morty,
+      summer
+    ];
+    const image = images[Math.floor(Math.random() * images.length)];
+    return <img src={image} alt="character" className="char" />;
+  };
+
+  randomImage = () => {
+    const greetings = [
+      "Hello",
+      "Peace on all worlds",
+      "Squanchy squanch",
+      "Greetings",
+      "Greetings Earthling"
+    ];
+    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+    return greeting;
+  };
+
+  randomGreeting = () => {
+    const greetings = [
+      "Hello",
+      "Peace on all worlds",
+      "Squanchy squanch",
+      "Greetings",
+      "Greetings Earthling"
+    ];
+    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+    return greeting;
+  };
+
   render() {
+    console.log(this.state);
+    const { name, zodiac, json } = this.state;
+    if (!name && !zodiac) {
+      return <Name start={this.getHoroscope} />;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <PageContainer>
+        <NameContainer>
+          {this.getImage()}
+          <h1>
+            {this.randomGreeting()} Earth-{json.lucky_number} {name}!
+          </h1>
+        </NameContainer>
+        <Horoscope>
+          <div>
+            It is {json.current_date} on Earth-{json.lucky_number}
+          </div>
+          <div>
+            intergalactic beings birthed between {json.date_range} should feel a
+            certain <u>{json.mood}</u> caused by their moons gravitational pull.
+            Your ideal mate is a {json.compatibility} from Earth-
+            {(Math.floor(Math.random() * 10) + 2) * json.lucky_number}
+          </div>
+          <div>A good time to take action is at {json.lucky_time}</div>
+          <div className="color">
+            The color <ColorBox color={json.color} /> should shield you from the
+            intergalactic federation and bring peace among worlds.
+          </div>
+        </Horoscope>
+        <Description>
+          <Bubble>{json.description}</Bubble>
+          <img src={gum} className="gum" alt="gum" />
+        </Description>
+      </PageContainer>
     );
   }
 }
 
-export default App;
+export default Main;
---
+++ src/name.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import Ufo from "./science/ufo";
+import posed from "react-pose";
+import { tween, easing, styler } from "popmotion";
+
+const PageContainer = styled.div`
+  display: flex;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  padding-top: 50px;
+  align-items: center;
+  align-content: center;
+  flex-direction: column;
+  background-image: linear-gradient(45deg, #e4a788 0%, #f0e14a 100%);
+  button {
+    margin-top: 20px;
+    background-color: #00a8ff; /* Green */
+    border: none;
+    color: #f5f6fa;
+    padding: 15px 32px;
+    text-align: center;
+    border-radius: 10px;
+    width: 50%;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+`;
+const Zodiacs = styled(Dropdown)`
+  width: 50%;
+  background: papayawhip;
+  border-radius: 5px;
+  .Dropdown-control,
+  .Dropdown-option {
+    background: #b7e4f9ff;
+    font-size: 23px;
+  }
+`;
+
+const NameInput = styled.input`
+  width: 50%;
+  height: 45px;
+  margin: 50px 0 25px 0;
+  padding: 5px;
+  color: "palevioletred";
+  background: #b7e4f9ff;
+  border: none;
+  font-size: 23px;
+  border-radius: 3px;
+`;
+
+const options = [
+  "Aries: Mar 20 – Apr 20",
+  "Taurus: Apr 20 – May 21",
+  "Gemini: May 21 – Jun 21",
+  "Cancer: Jun 21 – Jul 23",
+  "Leo: Jul 23 – Aug 23",
+  "Virgo: Aug 23 – Sep 23",
+  "Libra: Sep 23 – Oct 23",
+  "Scorpio: Oct 23 – Nov 22",
+  "Sagittarius: Nov 22 – Dec 22",
+  "Capricorn: Dec 22 – Jan 20",
+  "Aquarius: Jan 20 – Feb 18",
+  "Pisces: Feb 18 – Mar 20"
+];
+
+class Name extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      zodiac: null
+    };
+  }
+
+  beginButton = () => {
+    const { name, zodiac } = this.state;
+    const { start } = this.props;
+    // if (name && zodiac) {
+    if (name && zodiac) {
+      const divStyler = styler(document.querySelector(".ufo"));
+      tween({
+        from: 0,
+        to: { x: 1000, rotate: -30 },
+        duration: 1500,
+        ease: easing.backOut,
+        flip: 1,
+        yoyo: 1
+      }).start(divStyler.set);
+
+      return (
+        <button
+          onClick={() => {
+            start(this.state);
+          }}
+        >
+          Start
+        </button>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <PageContainer>
+        <Ufo width="25%" className="ufo" />
+        <NameInput
+          onChange={e => {
+            this.setState({ name: e.target.value });
+          }}
+          placeholder="Name"
+        />
+        <Zodiacs
+          options={options}
+          onChange={e => {
+            this.setState({ zodiac: e.value });
+          }}
+          value={"Zodiac"}
+          placeholder="Select an option"
+        />
+        {this.beginButton()}
+      </PageContainer>
+    );
+  }
+}
+
+export default Name;
